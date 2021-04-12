035b36705fa25e4ac528d8943d14e8ce2db523c3
Marcelo Ishii <marcelokoiti2014@gmail.com>
2021-04-12 07:37:20 -0300

    Main View

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,26 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+//Estamos instanciando ao inves de fazer de forma estatica. Possibilita manipular antes de carregar
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load();
+			Scene mainScene = new Scene(parent);                  // Cena principal
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Exemplo JavaFX application");
+			primaryStage.show();                                 // mostra o palco
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
